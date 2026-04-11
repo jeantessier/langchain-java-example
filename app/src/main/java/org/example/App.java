e758d7791e702551228783b8fbc31eb48bf66f70
Jean Tessier <jean@jeantessier.com>
2026-04-10 22:32:18 -0700

    Modernize code with var, records, and List.of()

--- app/src/main/java/org/example/App.java
+++ app/src/main/java/org/example/App.java
@@ -1,15 +1,11 @@
 package org.example;
 
 import com.google.inject.Inject;
-import dev.langchain4j.data.message.AiMessage;
 import dev.langchain4j.model.chat.ChatModel;
-import dev.langchain4j.model.input.Prompt;
 import dev.langchain4j.model.input.structured.StructuredPrompt;
 import dev.langchain4j.model.input.structured.StructuredPromptProcessor;
 
 import java.util.List;
-
-import static java.util.Arrays.asList;
 
 public class App {
     private final ChatModel chatModel;
@@ -24,18 +20,13 @@ public class App {
     }
 
     @StructuredPrompt("Create a recipe for a {{dish}} that can be prepared using only {{ingredients}}")
-    private static class CreateRecipePrompt {
-        private String dish;
-        private List<String> ingredients;
-    }
+    private record CreateRecipePrompt(String dish, List<String> ingredients) {}
 
     public String getRecipe() {
-        CreateRecipePrompt createRecipePrompt = new CreateRecipePrompt();
-        createRecipePrompt.dish = "salad";
-        createRecipePrompt.ingredients = asList("cucumber", "tomato", "feta", "onion", "olives");
-        Prompt prompt = StructuredPromptProcessor.toPrompt(createRecipePrompt);
+        var createRecipePrompt = new CreateRecipePrompt("salad", List.of("cucumber", "tomato", "feta", "onion", "olives"));
+        var prompt = StructuredPromptProcessor.toPrompt(createRecipePrompt);
 
-        AiMessage aiMessage = chatModel.chat(prompt.toUserMessage()).aiMessage();
+        var aiMessage = chatModel.chat(prompt.toUserMessage()).aiMessage();
 
         return aiMessage.text();
     }
--- app/src/main/java/org/example/Main.java
+++ app/src/main/java/org/example/Main.java
@@ -1,9 +1,7 @@
 package org.example;
 
 import com.google.inject.Guice;
-import com.google.inject.Injector;
 
-import java.util.Arrays;
 import java.util.List;
 
 public class Main {
@@ -13,19 +11,19 @@ public class Main {
     private static final String DEFAULT_MODEL = OPENAI_MODEL;
 
     public static void main(String[] args) {
-        List<String> argsList = Arrays.asList(args);
-        int modelNameIndex = argsList.indexOf("--model");
-        String modelNameValue = (modelNameIndex >= 0) ? argsList.get(modelNameIndex + 1) : DEFAULT_MODEL;
+        var argsList = List.of(args);
+        var modelNameIndex = argsList.indexOf("--model");
+        var modelNameValue = (modelNameIndex >= 0) ? argsList.get(modelNameIndex + 1) : DEFAULT_MODEL;
 
-        com.google.inject.Module aiModule = switch (modelNameValue) {
+        var aiModule = switch (modelNameValue) {
             case OPENAI_MODEL -> new OpenAiModule();
             case ANTHROPIC_MODEL -> new AnthropicModule();
             case GOOGLE_MODEL -> new GoogleModule();
             default -> throw new IllegalStateException("Unexpected value: " + modelNameValue);
         };
 
-        Injector injector = Guice.createInjector(new DotenvModule(), aiModule);
-        App app = injector.getInstance(App.class);
+        var injector = Guice.createInjector(new DotenvModule(), aiModule);
+        var app = injector.getInstance(App.class);
 
         System.out.println("Greeting");
         System.out.println("--------");
